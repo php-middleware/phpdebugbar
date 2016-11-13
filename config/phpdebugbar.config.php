c6443eb687869bb799b76538bea1c410e4b120dc
Witold Wasiczko <witold@wasiczko.pl>
2016-11-13 23:39:33 +0100

    Configurable dependencies

---
+++ config/phpdebugbar.config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'phpmiddleware' => [
+        'phpdebugbar' => [
+            'javascript_renderer' => [
+                'base_url' => '/phpdebugbar',
+            ],
+            'collectors' => [],
+            'storage' => '',
+        ],
+    ],
+];
