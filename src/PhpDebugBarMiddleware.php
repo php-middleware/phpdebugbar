0f9639628e497c43ebe7546fedccd7742ce1eccf
Sascha-Oliver Prolic <saschaprolic@googlemail.com>
2018-03-27 00:07:44 +0800

    Update PhpDebugBarMiddleware.php

--- src/PhpDebugBarMiddleware.php
+++ src/PhpDebugBarMiddleware.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
 use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Http\Uri;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\Response\HtmlResponse;
